ddee5f164e2dfecab21d2ed57ac990f67be2448f
shaune <contact@shaunewest.com>
2017-08-19 18:45:32 +1000

    Basic global page setup

---
+++ pages/_document.js
@@ -0,0 +1,43 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet, injectGlobal } from 'styled-components'
+
+injectGlobal`
+  body {
+    color: white; 
+    font-family: sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    font-style: normal;
+    overflow-x: hidden;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+export default class MyDocument extends Document {
+  render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+    return (
+      <html>
+        <Head>
+          <title>Insults</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {styleTags}
+        </Head>
+        <body>
+          <div className="root">
+            {main}
+          </div>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
